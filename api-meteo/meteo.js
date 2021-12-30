748ce2f35ec34508caa329b5de2c7aac8eea37dc
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2021-12-30 17:04:27 +0100

    mise en place focntion search

--- api-meteo/meteo.js
+++ api-meteo/meteo.js
@@ -1,49 +1,63 @@
-alert('Ceci est mon premier script utilisant les API')
+alert("Ceci est mon premier script utilisant les API");
 
 function main() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 ")
-                .then(res=>res.json())
-                .then(data=>{
-                    console.log(data.main.temp)
-                    document.querySelector("#debug").innerText =Math.round(data.main.temp )                   
-                })
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 "
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.main.temp);
+      document.querySelector("#debug").innerText = Math.round(data.main.temp);
+    });
 }
 
-        window.fetch("https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 ")
-                .then(res=>res.json())
-                .then(data=>{
-                  console.log(data.weather[0].description )
-                  document.querySelector("#cond").innerText =data.weather[0].description
-              })
-                
-                window.fetch("https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 ")
-                .then(res=>res.json())
-                .then(data=>{
-                  console.log(data.weather[0].icon )
-                  document.querySelector("#icone").innerText =data.weather[0].icon
-              })
-                
-        window.fetch("https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 ")
-                .then(res=>res.json())
-                .then(data=>{
-                    console.log(data.name )
-                    document.querySelector("#city").innerText =data.name
-                   
-              })
-                
-                window.fetch("https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 ")
-                .then(res=>res.json())
-                .then(data=>{
-                    console.log(data.wind.speed )
-                    document.querySelector("#wind").innerText =data.wind.speed
-                   
-              })
+window
+  .fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 "
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.weather[0].description);
+    document.querySelector("#cond").innerText = data.weather[0].description;
+  });
 
-window.fetch("https://geo.api.gouv.fr/communes?nom=Soissons&fields=population&format=json&geometry=centre ")
-                .then(res=>res.json())
-                .then(data=>{
-                    console.log(data.nom)
-                    document.querySelector("#pop").innerText =data.nom
-                   
-              })                              
+window
+  .fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 "
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.weather[0].icon);
+    document.querySelector("#icone").innerText = data.weather[0].icon;
+  });
+
+window
+  .fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 "
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.name);
+    document.querySelector("#city").innerText = data.name;
+  });
+
+window
+  .fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Soissons&lang=fr&units=metric&appid=8417760ba8724050ae99eca6e6caa318 "
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.wind.speed);
+    document.querySelector("#wind").innerText = data.wind.speed;
+  });
+
+window
+  .fetch(
+    "https://geo.api.gouv.fr/communes?nom=Soissons&fields=population&format=json&geometry=centre "
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.nom);
+    document.querySelector("#pop").innerText = data.nom;
+  });
 main();
--- eye.js
+++ eye.js
@@ -1,13 +1,32 @@
-let balls = document.getElementsByClassName('ball');
+let balls = document.getElementsByClassName("ball");
 
 document.onmousemove = function () {
-    var x  = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
+  var x = (event.clientX * 100) / window.innerWidth + "%";
+  var y = (event.clientY * 100) / window.innerHeight + "%";
 
-    for (let i = 0; i < 2; i++) {
-        balls[i].style.left = x;
-        balls[i].style.top = y;
-        balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
-    }
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
+  }
+};
 
+function openPage() {
+  const a = document.getElementById("search1").value;
+
+  if (a === "CV") {
+    window.open("/cv/cyril-gourdon.pdf");
+  }
+
+  if (a === "Bootstrap") {
+    window.open("/intranet/intranet.html");
+  }
+
+  if (a === "flexbox") {
+    window.open("/flexbox/defi.html");
+  }
+
+  if (a === "Grid") {
+    window.open("/grid/defi.html");
+  }
 }
